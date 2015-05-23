6bbd034eb3d0e46d035012b898a45a40eff41708
sajiddanish <sajiddanish14@yahoo.com>
2015-05-23 06:12:21 +0000

    create user and jobs table and removed spring

---
+++ db/migrate/20150523043726_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :phone
+      t.text :experstise
+      t.string :experience 
+      t.text :skills
+      t.string :location
+      t.timestamps
+    end
+  end
+end
